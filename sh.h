dd71c3e649af02ec89f2ca97d95f658789bd20f3
Jack Sabagh <79114835+djsabagh@users.noreply.github.com>
2021-10-10 21:49:40 -0400

    update #1

--- sh.h
+++ sh.h
@@ -1,12 +1,11 @@
-
 #include "get_path.h"
 
 int pid;
 int sh( int argc, char **argv, char **envp);
 char *which(char *command, struct pathelement *pathlist);
 char *where(char *command, struct pathelement *pathlist);
-void list ( char *dir );
+void listPrint( char *d );
 void printenv(char **envp);
-
+void listFree(struct pathelement *head);
 #define PROMPTMAX 32
 #define MAXARGS 10
